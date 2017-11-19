8c9d93967a2fdbcb3d85a4a9fb3ab72ea862d306
bioinfochicks <christine.hicks@sch.nhs.uk>
2017-11-19 17:19:38 +0000

    made into get_well_info() method

--- nbs_bulk_upload.py
+++ nbs_bulk_upload.py
@@ -11,15 +11,19 @@ import xml.etree.ElementTree as ET
 tree = ET.parse("C:\\Users\\chris\\Desktop\\NBSUpload\\1234567.p9.txt")
 root = tree.getroot()
 
-    # Loop for find children under fixed_annotation
+# create dictionary 
 d = {}
-    
-for child in root.findall('Wells'):
-    for well in child.iter('Well'):
-        d1 = {}
-        d1['SampleBarcode'] = well.find('SampleBarcode').text
-        d1['IsBad'] = well.find('IsBad').text
-        d1['RequestedDisksPerWell'] = well.find('RequestedDisksPerWell').text
-        d1['PunchedDisksPerWell'] = well.find('PunchedDisksPerWell').text
-        d[well.find('Index').text] = d1
-print(d)
+#get well info for each well and put into dict with index as the key and the values in another dict
+def get_well_info(root):
+    for child in root.findall('Wells'):
+        for well in child.iter('Well'):
+            d1 = {}
+            d1['SampleBarcode'] = well.find('SampleBarcode').text
+            d1['IsBad'] = well.find('IsBad').text
+            d1['RequestedDisksPerWell'] = well.find('RequestedDisksPerWell').text
+            d1['PunchedDisksPerWell'] = well.find('PunchedDisksPerWell').text
+            d[well.find('Index').text] = d1
+            
+get_well_info(root)
+print(d['0']['SampleBarcode'])
+
