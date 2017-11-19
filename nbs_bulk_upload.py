4bb0b44f7feed8c619199ba3d59cbc83c6f2cfb2
bioinfochicks <christine.hicks@sch.nhs.uk>
2017-11-19 17:23:21 +0000

    editted get_well_info() method

--- nbs_bulk_upload.py
+++ nbs_bulk_upload.py
@@ -11,19 +11,26 @@ import xml.etree.ElementTree as ET
 tree = ET.parse("C:\\Users\\chris\\Desktop\\NBSUpload\\1234567.p9.txt")
 root = tree.getroot()
 
-# create dictionary 
-d = {}
+
 #get well info for each well and put into dict with index as the key and the values in another dict
 def get_well_info(root):
+    """
+    method to get well info for each well and put into dict with index as the key and the values in another dict
+    Input: root of element tree
+    Return: Dictionary of dictionaries containing well information, with the index and the primary key
+    """
+    # create dictionary 
+    d = {}
     for child in root.findall('Wells'):
-        for well in child.iter('Well'):
-            d1 = {}
-            d1['SampleBarcode'] = well.find('SampleBarcode').text
-            d1['IsBad'] = well.find('IsBad').text
-            d1['RequestedDisksPerWell'] = well.find('RequestedDisksPerWell').text
-            d1['PunchedDisksPerWell'] = well.find('PunchedDisksPerWell').text
-            d[well.find('Index').text] = d1
+       for well in child.iter('Well'):
+           d1 = {}
+           d1['SampleBarcode'] = well.find('SampleBarcode').text
+           d1['IsBad'] = well.find('IsBad').text
+           d1['RequestedDisksPerWell'] = well.find('RequestedDisksPerWell').text
+           d1['PunchedDisksPerWell'] = well.find('PunchedDisksPerWell').text
+           d[well.find('Index').text] = d1
+    return(d)
             
-get_well_info(root)
-print(d['0']['SampleBarcode'])
+test = get_well_info(root)
+print(test['0']['SampleBarcode'])
 
